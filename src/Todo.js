d998f1e3795433fa1a899605d73ffd548d93c901
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-05-07 13:44:15 +0200

    update button

--- src/Todo.js
+++ src/Todo.js
@@ -2,17 +2,27 @@ import React from "react";
 import "./Todo.css";
 
 export default function Todo() {
+  let [newword, setNeword] = useState("");
+  function listupdate(event) {
+    event.preventDefault();
+  }
+
+  function handleNewWord(event) {
+    setNeword(event.target.value);
+  }
   return (
     <div className="Todo">
       <div className="entry">
-        <form>
+        <form onSubmit={listupdate}>
           <div class="input-group mb-3">
             <input
               type="text"
               class="form-control"
               placeholder="New"
               autoComplete="off"
+              onChange={handleNewWord}
             />
+            <button class="btn btn-outline-secondary">Update</button>
           </div>
         </form>
       </div>
